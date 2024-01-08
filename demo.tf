8b0317e4d32f17bc9370c09618b9372331df0804
*n6pPlj4ps4 <s.tanaka@atomitech.jp>
2024-01-08 12:37:51 +0900

    微調整

--- demo.tf
+++ demo.tf
@@ -5,14 +5,12 @@ variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
 variable "private_key" {}
-variable "private_key_password" {}
 
 provider "oci" {
   tenancy_ocid = var.tenancy_ocid
   user_ocid = var.user_ocid
   fingerprint = var.fingerprint
   private_key = var.private_key
-  private_key_password = var.private_key_password
   region = var.region
 }
 
